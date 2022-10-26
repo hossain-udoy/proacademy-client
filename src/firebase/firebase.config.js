1801de36c2f1c72d1b7cf5682b9fe5b113a7c534
hossain-udoy <udoyh305@gmail.com>
2022-10-26 17:24:06 +0600

    google login and hot-toast installed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,16 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.React_App_apiKey,
+//     authDomain: process.env.React_App_authDomain,
+//     projectId: process.env.React_App_projectId,
+//     storageBucket: process.env.React_App_storageBucket,
+//     messagingSenderId: process.env.React_App_messagingSenderId,
+//     appId: process.env.React_App_appId,
+// };
 const firebaseConfig = {
-    apiKey: process.env.React_App_apiKey,
-    authDomain: process.env.React_App_authDomain,
-    projectId: process.env.React_App_projectId,
-    storageBucket: process.env.React_App_storageBucket,
-    messagingSenderId: process.env.React_App_messagingSenderId,
-    appId: process.env.React_App_appId,
-
+    apiKey: "AIzaSyDWXohUnlmVP4tmumjzkusKxxoGr9iXgI8",
+    authDomain: "proacademy-304a9.firebaseapp.com",
+    projectId: "proacademy-304a9",
+    storageBucket: "proacademy-304a9.appspot.com",
+    messagingSenderId: "338010946510",
+    appId: "1:338010946510:web:863b2596e0aa4375c9899f"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css'
+import './Login.css';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { useContext } from 'react';
 
 const Login = () => {
+    const { providerLogin, verifyEmail } = useContext(AuthContext);
+
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                verifyEmail();
+                console.log(user);
+            })
+            .catch(error => console.error(error))
+    }
     return (
         <div className='bg-img'>
             <div className="login-wrapper">
@@ -25,7 +41,7 @@ const Login = () => {
 
                     <div className='flex flex-col text-center'>
                         <button className='mb-2 bg-transparent hover:bg-violet-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-3 ml-3 w-64 shadow-md'><Link to='/register'>Register</Link></button>
-                        <button className='mb-2 bg-transparent hover:bg-violet-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-3 ml-3 w-64 shadow-md'>Continue with Google</button>
+                        <button onClick={handleGoogleSignIn} className='mb-2 bg-transparent hover:bg-violet-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-3 ml-3 w-64 shadow-md'>Continue with Google</button>
                         <button className='mb-2 bg-transparent hover:bg-violet-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-3 ml-3 w-64 shadow-md'>Continue with Github</button>
                     </div>
                 </form>
