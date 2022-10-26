7803afdecb83055e9ef1b9a5238dd78392f0017e
hossain-udoy <udoyh305@gmail.com>
2022-10-26 06:56:56 +0600

    Blog pages are done

---
+++ src/others/Questions.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Item = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="border rounded shadow-sm">
+            <button
+                type="button"
+                aria-label="Open item"
+                title="Open item"
+                className="flex items-center justify-between w-full p-4 focus:outline-none"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <p className="text-lg font-medium">{title}</p>
+                <div className="flex items-center justify-center w-8 h-8 border rounded-full">
+                    <svg
+                        viewBox="0 0 24 24"
+                        className={`w-3 text-gray-600 transition-transform duration-200 ${isOpen ? 'transform rotate-180' : ''
+                            }`}
+                    >
+                        <polyline
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            points="2,7 12,17 22,7"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                </div>
+            </button>
+            {isOpen && (
+                <div className="p-4 pt-0">
+                    <p className="text-white">{children}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+const Questions = () => {
+    return (
+        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
+                <div class="flex flex-col mb-16 sm:text-center">
+                    <a href="/" class="mb-6 sm:mx-auto">
+
+                    </a>
+                    <div class="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
+                        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                            <span class="relative inline-block">
+
+                                <span class="relative">Here</span>
+                            </span>{' '}
+                            Are Some Faq's
+                        </h2>
+                        <p class="text-base text-gray-700 md:text-lg">
+                            You can give us your questions through newsletter.We will try to answer.
+                        </p>
+                    </div>
+                </div>
+                <div class="space-y-4">
+                    <Item title="What is cors?">
+                        Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served. A web page may freely embed cross-origin images, stylesheets, scripts, iframes, and videos.
+                    </Item>
+                    <Item title="Why are you using firebase?">
+                        Firebase is great for quick projects: it's easy to set up, fast, in many cases requires only front-end logic. It lets you focus on your app instead of implementing custom authentication, web sockets or database connections.
+                    </Item>
+                    <Item title="What other options do you have to implement authentication?">
+                        Common Authentication Types
+                        a) Password-based authentication. Passwords are the most common b)methods of authentication. ...
+                        c)Multi-factor authentication. ...
+                        d)Certificate-based authentication. ...
+                        e)Biometric authentication. ...
+                        f)Token-based authentication.
+                    </Item>
+                    <Item title="What is Node? How does Node work?">
+                        It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+                    </Item>
+                    <Item title="How does the private route work?">
+                        The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property.
+                    </Item>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Questions;
--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,41 +1,38 @@
 import React from 'react';
+import Questions from '../../others/Questions';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 const Blog = () => {
     return (
-        <div>
+        <div className='bg'>
 
 
 
-            <div class="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
-                <div class="w-full lg:w-1/2">
-                    <div class="lg:max-w-lg">
-                        <h1 class="text-3xl font-bold tracking-wide text-gray-800 dark:text-white lg:text-5xl">
+            <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
+                <div className="w-full lg:w-1/2">
+                    <div className="lg:max-w-lg">
+                        <h1 className="text-3xl font-bold tracking-wide text-gray-800 dark:text-white lg:text-5xl">
                             Easiest way to create your website
                         </h1>
 
-                        <div class="mt-8 space-y-5">
-                            <p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
+                        <div className="mt-8 space-y-5">
+                            <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
 
-                                <span class="mx-2">Clean and Simple Layout</span>
+                                <AiOutlineCheckCircle className='text-white text-xl'></AiOutlineCheckCircle>
+
+                                <span className="mx-2 text-black text-xl">Clean and Simple Layout</span>
                             </p>
 
-                            <p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
+                            <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+                                <AiOutlineCheckCircle className='text-white text-xl'></AiOutlineCheckCircle>
 
-                                <span class="mx-2">Just Copy Paste Codeing</span>
+                                <span className="mx-2 text-black text-xl">Just Copy Paste Codeing</span>
                             </p>
 
-                            <p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
+                            <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+                                <AiOutlineCheckCircle className='text-white text-xl'></AiOutlineCheckCircle>
 
-                                <span class="mx-2">Easy to Use</span>
+                                <span className="mx-2 text-black text-xl">Easy to Use</span>
                             </p>
                         </div>
                     </div>
@@ -43,10 +40,15 @@ const Blog = () => {
 
                 </div>
 
-                <div class="flex items-center justify-center w-full h-96 lg:w-1/2">
+                <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
                     <img className="w-full h-full mx-auto rounded-md lg:max-w-2xl" src="https://images.unsplash.com/photo-1543269664-7eef42226a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
                 </div>
             </div>
+
+            <div>
+                <Questions></Questions>
+            </div>
+
 
         </div>
 
